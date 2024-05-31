b8262485c555aa931cd9f99ce7d89735976e27bc
adityaab13 <adityabadiger1997@gmail.com>
2024-05-30 22:47:56 -0400

    Comments added

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -7,17 +7,21 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+// NewsList component to display the list of news items
 const NewsList = observer(() => {
   const { newsStore } = useStore();
-
+  
+  // Fetch news items when the component mounts
   useEffect(() => {
     newsStore.fetchNews();
   }, [newsStore]);
 
+  // Scroll to top when the current page changes
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [newsStore.currentPage]);
 
+  // Show loading indicator if data is being fetched
   if (newsStore.loading && newsStore.items.length === 0) {
     return (
       <Box display="flex" justifyContent="center" mt={2}>
@@ -26,6 +30,7 @@ const NewsList = observer(() => {
     );
   }
 
+  // Show error message if there is an error
   if (newsStore.error) {
     return (
       <Box display="flex" justifyContent="center" mt={2}>
@@ -36,14 +41,17 @@ const NewsList = observer(() => {
 
   return (
     <Box>
+      {/* Show loading indicator if data is being fetched */}
       {newsStore.loading && (
         <Box display="flex" justifyContent="center" mt={2}>
           <CircularProgress />
         </Box>
       )}
+      {/* Render the list of news cards */}
       {newsStore.filteredItems.map((item) => (
         <NewsCard key={item.id} {...item} />
       ))}
+      {/* Render the pagination component */}
       <PaginationComponent />
     </Box>
   );
